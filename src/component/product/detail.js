e567d3cb08eaf77368ac4f1e6827920d060f530b
Prajal Shrestha <shreprajal@gmail.com>
2025-03-03 12:44:00 +0545

    dependency updated

--- src/component/product/detail.js
+++ src/component/product/detail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router";
 import "./details.css";
 
 const ProductDetail = () => {
